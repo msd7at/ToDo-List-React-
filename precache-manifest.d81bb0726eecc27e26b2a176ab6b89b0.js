ecef5e84d744d852cb66d6ad69d0127dc3037267
Anurag Tiwari <52295974+msd7at@users.noreply.github.com>
2020-09-15 11:43:20 +0530

    Updates

---
+++ precache-manifest.d81bb0726eecc27e26b2a176ab6b89b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049645880f30f01d28209471a4d33df1",
+    "url": "/ToDo-List-React-/index.html"
+  },
+  {
+    "revision": "1e3bcf0168fdcb89b875",
+    "url": "/ToDo-List-React-/static/css/main.f063349c.chunk.css"
+  },
+  {
+    "revision": "2a25b4d7158a8c1aee6c",
+    "url": "/ToDo-List-React-/static/js/2.29914bca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo-List-React-/static/js/2.29914bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3bcf0168fdcb89b875",
+    "url": "/ToDo-List-React-/static/js/main.c9e30118.chunk.js"
+  },
+  {
+    "revision": "230c05845055c6472a12",
+    "url": "/ToDo-List-React-/static/js/runtime-main.1524d165.js"
+  }
+]);
